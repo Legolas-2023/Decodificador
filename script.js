7f348e8221f5a69e99c9f83c34fb71f4df573347
Crystian <151743336+Legolas-2023@users.noreply.github.com>
2024-02-19 15:49:37 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let area1 = document.getElementById('text_enter');
-let area2 = document.getElementById('area');
-let btcopiar = document.getElementById('btn_copy');
-let remover = document.getElementById('group');
+let area1 = document.getElementById('secao-criptografa');
+let area2 = document.getElementById('secao-descriptografar');
+let remove = document.getElementById('remover');
+let removestilo = document.querySelector('section.descriptografar');
+let botaocopiar = document.querySelector('button.copiar');
 
-   
 function criptografar(){
     if(area1.value == ''){
         alert('Insira algum texto a ser criptografado!');
@@ -12,8 +12,11 @@ function criptografar(){
         area1.value = '';
         return;
     }else{
-        btcopiar.style.display = 'block';
-        remover.style.display = 'none';
+        remove.style.display = 'none';
+        removestilo.style.justifyContent = 'flex-start';
+        area2.style.display = 'block';
+        botaocopiar.style.display = 'block';
+
         let text = area1.value.toLowerCase();
         const words = {
             a: "ai",
@@ -26,6 +29,7 @@ function criptografar(){
         const reg = new RegExp(Object.keys(words).join("|"), "g");
         let result = text.replaceAll(reg, (matched) => words[matched]);
         area2.innerHTML = result;
+
     }
     return check
 }
@@ -45,7 +49,11 @@ function descriptografar(){
 }
 
 function copiar(){
-    let copyText = area2;
-    navigator.clipboard.writeText(copyText.value).then(copiar => alert("Texto copiado para a área de transferência."));
-    area1.value = '';
-};
+    if(area2.value == ''){
+        alert('Insira algum texto a ser criptografado para ser copiado!');
+    }else{
+        let copyText = area2;
+        navigator.clipboard.writeText(copyText.value).then(copiar => alert("Texto copiado para a área de transferência."));
+        area1.value = '';
+    }
+}
